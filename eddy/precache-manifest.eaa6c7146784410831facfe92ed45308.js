7eccc61f08f54bfa875a7a322c0b81321133ab25
ilandn <ilandayans@gmail.com>
2020-10-15 13:40:55 -0500

    prod

---
+++ eddy/precache-manifest.eaa6c7146784410831facfe92ed45308.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6c6b8edc4da92bd9851",
+    "url": "./static/js/main.c6c6b8ed.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "144aac2d1e5b7651bfa4",
+    "url": "./static/css/2.94a4527a.chunk.css"
+  },
+  {
+    "revision": "144aac2d1e5b7651bfa4",
+    "url": "./static/js/2.144aac2d.chunk.js"
+  },
+  {
+    "revision": "9c861c7c073ada9d4bb4394d60b93a10",
+    "url": "./static/media/Marketplace.9c861c7c.jpg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "./static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "./static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "./static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "./static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "./static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "3294b20519a4e34affe03b972b69c3c2",
+    "url": "./index.html"
+  }
+];
